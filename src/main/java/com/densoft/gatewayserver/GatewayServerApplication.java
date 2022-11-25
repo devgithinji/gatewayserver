f656a16820862fe867bf557913ab8e77510f8cdc
dennis githinji <wakahiad@gmail.com>
2022-11-25 19:44:39 +0300

    bug fix

--- src/main/java/com/densoft/gatewayserver/GatewayServerApplication.java
+++ src/main/java/com/densoft/gatewayserver/GatewayServerApplication.java
@@ -17,20 +17,23 @@ public class GatewayServerApplication {
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes().route(p -> p
+        return builder.routes()
+                .route(p -> p
                         .path("/eazybank/accounts/**")
                         .filters(f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", new Date().toString())).uri("1b://ACCOUNTS"))
-                .route(p -> p
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://ACCOUNTS")).
+                route(p -> p
                         .path("/eazybank/loans/**")
                         .filters(f -> f.rewritePath("/eazybank/loans/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", new Date().toString())).uri("1b://LOANS"))
-                .route(p -> p
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://LOANS")).
+                route(p -> p
                         .path("/eazybank/cards/**")
                         .filters(f -> f.rewritePath("/eazybank/cards/(?<segment>.*)", "/${segment}")
-                                .addResponseHeader("X-Response-Time", new Date().toString())).uri("1b://CARDS")).build();
+                                .addResponseHeader("X-Response-Time", new Date().toString()))
+                        .uri("lb://CARDS")).build();
     }
-
 
 
 }
